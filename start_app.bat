9d4458e3398a2a70eee8474be830bcd3d6169b23
Albert Balbekov <albert.y.balbekov@gmail.com>
2019-02-03 22:03:54 -0500

    Fixes to sqls to convert between browser and OS database timezone.
    Changed date format in sqlmonitor select from 12hr to 24hr.
    
    GV$SQLMONITOR select, DBA_HIST_SYSMETRIC_HISORY select and DBA_HIST_SYSMETRIC_SUMMARY select
    now convert from browser time zone to database OS timezone in where clauses.
    
    SQL now return dates in browser time zone.
    
    jQuery datatable now scrolls header when table is horizontally scrolled.

--- start_app.bat
+++ start_app.bat
@@ -4,7 +4,7 @@ rem
 docker run -d --rm ^
 --name app ^
 -p 0.0.0.0:5000:5000 ^
--v %CD%/dbhist_viewer:/app ^
+-v "%CD%\dbhist_viewer":/app ^
 -e db_ora122="system/Oradoc_db1@oradb:1521/ORCLCDB.localdomain" ^
 -e PYTHONUNBUFFERED=1 ^
 --link ora122:oradb ^
